139e5a3ba23af5e53074929bf009b873e2bb2a13
DesignIvanX <74554535+DesignIvanX@users.noreply.github.com>
2025-04-04 00:26:43 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,499 @@
+let productos =  [
+    {
+        "id": "abrigos-01",
+        "titulo": "Vans Old School",
+        "imagen": "./img/premiun/vans-old-school.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 400.000
+    },
+    {
+        "id": "abrigo-02",
+        "titulo": "Campus Pro Beige",
+        "imagen": "./img/premiun/campus-pro-beige.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 400.000
+    },
+    {
+        "id": "abrigo-03",
+        "titulo": "Vans Old School Blue",
+        "imagen": "./img/premiun/vans-old-school-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-04",
+        "titulo": "Dunk Cinza",
+        "imagen": "./img/premiun/dunk-cinza.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-05",
+        "titulo": "Dunk Cacao",
+        "imagen": "./img/premiun/dunk-cacao.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-06",
+        "titulo": "Dunk Full Black",
+        "imagen": "./img/premiun/dunk-full-black.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-07",
+        "titulo": "Dunk White Navy Pro",
+        "imagen": "./img/premiun/dunk-white-navy-pro.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-08",
+        "titulo": "Dunk Los Angeles Dodgers",
+        "imagen": "./img/premiun/dunk-los-angeles-dodgers.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-09",
+        "titulo": "Dunk Panda",
+        "imagen": "./img/premiun/dunk-panda.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-10",
+        "titulo": "Nike Dunk Full White",
+        "imagen": "./img/premiun/nike-dunk-full-white.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-11",
+        "titulo": "Dunk Bordeaux",
+        "imagen": "./img/premiun/dunk-bordeaux.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-12",
+        "titulo": "Dunk Grey",
+        "imagen": "./img/premiun/dunk-grey.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-13",
+        "titulo": "Nike Blazer",
+        "imagen": "./img/premiun/nike-blazer.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-14",
+        "titulo": "KNU White",
+        "imagen": "./img/premiun/knu-white.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-15",
+        "titulo": "KNU Reverse",
+        "imagen": "./img/premiun/knu-reverse.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-16",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-17",
+        "titulo": "KNU Preto",
+        "imagen": "./img/premiun/knu-preto.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    
+    {
+        "id": "abrigo-18",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-19",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-20",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-21",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-22",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-23",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "abrigo-24",
+        "titulo": "Knu Light Blue",
+        "imagen": "./img/premiun/knu-light-blue.jpg",
+        "categoria": {
+            "nombre": "Premium",
+            "id": "abrigos"
+        },
+        "precio": 1000
+    },
+    
+    
+    
+
+
+
+
+    
+    {
+        "id": "camiseta-01",
+        "titulo": "Camiseta 01",
+        "imagen": "./img/camisetas/01.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-02",
+        "titulo": "Camiseta 02",
+        "imagen": "./img/camisetas/02.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-03",
+        "titulo": "Camiseta 03",
+        "imagen": "./img/camisetas/03.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-04",
+        "titulo": "Camiseta 04",
+        "imagen": "./img/camisetas/04.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-05",
+        "titulo": "Camiseta 05",
+        "imagen": "./img/camisetas/05.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-06",
+        "titulo": "Camiseta 06",
+        "imagen": "./img/camisetas/06.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-07",
+        "titulo": "Camiseta 07",
+        "imagen": "./img/camisetas/07.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-08",
+        "titulo": "Camiseta 08",
+        "imagen": "./img/camisetas/08.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-01",
+        "titulo": "Pantalón 01",
+        "imagen": "./img/pantalones/01.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-02",
+        "titulo": "Pantalón 02",
+        "imagen": "./img/pantalones/02.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-03",
+        "titulo": "Pantalón 03",
+        "imagen": "./img/pantalones/03.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-04",
+        "titulo": "Pantalón 04",
+        "imagen": "./img/pantalones/04.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-05",
+        "titulo": "Pantalón 05",
+        "imagen": "./img/pantalones/05.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    }
+]
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">GS. ${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
